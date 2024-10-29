6c8f3d5fd1a3dadbd2efaad79c1b666f561b641f
Viniciusgithu <viniciuslimavirtuel@email.com>
2024-10-29 15:24:49 -0300

    update config navbar and menuhamburguer

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,45 +1,71 @@
 'use client'
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import InfoNavBar from './InfoNavbar';
 import Logo from './Logo';
-import SocialMedia from './SocialMedia';
+import LogoSecondary from './1. Logo_Principal_Horizontal_Pague_Bem_Brasil.png'; // Nova logo ao rolar a página
+// import SocialMedia from './SocialMedia';
 import { FaBars, FaTimes } from 'react-icons/fa';
 import ButtonPrimary from './ButtonPrimary';
 import ButtonSecondary from './ButtonSecondary';
+import Link from 'next/link';
 
 export default function Navbar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isScrolled, setIsScrolled] = useState(false);
 
+  // Função para alternar o menu
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
-  return (
-    <nav className="bg-gradient-to-r from-[#2D4416] via-[#39670B] to-[#71AA37] absolute inset-x-0 top-0 z-50 p-4 flex items-center justify-between">
+  // Efeito para monitorar a rolagem da página
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 50) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
 
-      
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return (
+    <nav
+      className={`fixed inset-x-0 top-0 z-50 p-4 flex items-center justify-between transition-colors duration-300 ${
+        isScrolled ? 'bg-white shadow-md' : 'bg-gradient-to-r from-[#2D4416] via-[#39670B] to-[#71AA37]'
+      }`}
+    >
       <div className="flex items-center justify-between w-full">
         {/* Logo */}
         <div className="flex items-center">
-          <Logo />
+          {isScrolled ? <img src="./1. Logo_Principal_Horizontal_Pague_Bem_Brasil.png" alt='' width={150} height={150}/> : <Logo />} {/* Alternar logo */}
         </div>
 
         {/* Menu Desktop */}
         <div className="hidden lg:flex items-center space-x-6 ml-10">
-          <InfoNavBar />
+          {/* <InfoNavBar />  */}
+          <InfoNavBar textColor={isScrolled ? 'text-green-800' : 'text-white'} hoverColor="hover:text-green-600" />
         </div>
 
-       
+        {/* Botões de Ação */}
         <div className="hidden lg:flex items-center space-x-4 ml-auto">
-        <ButtonPrimary text="ACESSAR CONTA" />
-        <ButtonSecondary text="CRIAR CONTA" />
+          <ButtonPrimary text="ACESSAR CONTA" />
+          <ButtonSecondary text="CRIAR CONTA" />
         </div>
       </div>
 
       {/* Botão de Menu para telas pequenas */}
       <button
-        className="lg:hidden text-white focus:outline-none"
+        className={`lg:hidden focus:outline-none ${
+          isScrolled ? 'text-gray-900' : 'text-white'
+        }`}
         onClick={toggleMenu}
         aria-label={isMenuOpen ? 'Fechar menu' : 'Abrir menu'}
       >
@@ -48,23 +74,44 @@ export default function Navbar() {
 
       {/* Menu Mobile */}
       {isMenuOpen && (
-        <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-end z-40">
-          <div className="bg-slate-100 w-64 p-6 flex flex-col items-center rounded-l-3xl">
+        <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-40">
+          <div className="bg-green-900 bg-opacity-90 p-6 rounded-lg max-h-screen w-4/5 md:w-1/3 flex flex-col items-center justify-center">
             <button
-              className="text-green-500 self-end focus:outline-none"
+              className="text-white self-end focus:outline-none"
               onClick={toggleMenu}
               aria-label="Fechar menu"
             >
               <FaTimes size={24} />
             </button>
+
             <div className="mt-4 space-y-4 text-center flex flex-col items-center">
-              <InfoNavBar />
+              {/* Links de navegação */}
+              <div className="flex flex-col items-center justify-center space-y-4">
+                <Link href="/quem-somos" className="px-3 py-2 text-sm font-bold text-white">
+                  Quem somos
+                </Link>
+                <Link href="/solucoes" className="px-3 py-2 text-sm font-bold text-white">
+                  Soluções
+                </Link>
+                <Link href="/planos" className="px-3 py-2 text-sm font-bold text-white">
+                  Planos
+                </Link>
+                <Link href="/contato" className="px-3 py-2 text-sm font-bold text-white">
+                  Contato
+                </Link>
+                <Link href="/depoimentos" className="px-3 py-2 text-sm font-bold text-white">
+                  Depoimentos
+                </Link>
+              </div>
+
+              {/* Botões de ação */}
               <ButtonPrimary text="ACESSAR CONTA" />
               <ButtonSecondary text="CRIAR CONTA" />
             </div>
           </div>
         </div>
       )}
+
     </nav>
   );
 }
--- src/components/InfoNavbar.tsx
+++ src/components/InfoNavbar.tsx
@@ -1,101 +1,58 @@
 'use client'
 
-
 import Link from 'next/link'
 import { useState } from 'react';
 
-export default function InfoNavBar(){
+interface InfoNavBarProps {
+  textColor?: string;
+  hoverColor?: string;
+}
+
+
+export default function InfoNavBar({ textColor = 'text-white', hoverColor = 'hover:text-green-600' }: InfoNavBarProps) {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
     <main className="flex items-center justify-center">
-    
-      {/* Logo PagueBem */}
-    <section className="flex space-b">
 
-
-
-
+      <section className="flex space-b">
         <div className="flex items-center justify-center">
-          <Link href="/quem-somos" className="text-white hover:text-green-600 px-3 py-2 text-sm font-bold">
+          <Link href="/quem-somos" className={`${textColor} ${hoverColor} px-3 py-2 text-sm font-bold`}>
             Quem somos
           </Link>
-          <Link href="/solucoes" className="text-white hover:text-green-600 px-3 py-2 text-sm font-bold">
+          <Link href="/solucoes" className={`${textColor} ${hoverColor} px-3 py-2 text-sm font-bold`}>
             Soluções
           </Link>
-          <Link href="/planos" className="text-white hover:text-green-600 px-3 py-2 text-sm font-bold">
+          <Link href="/planos" className={`${textColor} ${hoverColor} px-3 py-2 text-sm font-bold`}>
             Planos
           </Link>
-          <Link href="/contato" className="text-white hover:text-green-600 px-3 py-2 text-sm font-bold">
+          <Link href="/contato" className={`${textColor} ${hoverColor} px-3 py-2 text-sm font-bold`}>
             Contato
           </Link>
-          <Link href="/depoimentos" className="text-white hover:text-green-600 px-3 py-2 text-sm font-bold">
+          <Link href="/depoimentos" className={`${textColor} ${hoverColor} px-3 py-2 text-sm font-bold`}>
             Depoimentos
           </Link>
         </div>
+      </section>
 
-
-
-    </section>
-
-    {/* Buttons Criar Conta e Acessar */}
-    {/* <section className="hidden sm:ml-6 sm:flex sm:items-center">
-      <Link href="/criar-conta" className="bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded-md text-sm font-medium">
-        Criar conta
-      </Link>
-      <Link href="/acessar-conta" className="ml-4 text-gray-900 hover:text-green-600 px-3 py-2 text-sm font-medium">
-        Acessar conta
-      </Link>
-    </section> */}
-
-
-    <div className="flex items-center sm:hidden">
-      <button
-        onClick={() => setIsOpen(!isOpen)}
-        className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-green-500"
-      >
-        <span className="sr-only">Open main menu</span>
-        {isOpen ? (
-          <svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
-          </svg>
-        ) : (
-          <svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
-          </svg>
-        )}
-      </button>
-    </div>
-  </main>
-  )
+      <section className="flex items-center sm:hidden">
+        <button
+          onClick={() => setIsOpen(!isOpen)}
+          className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-green-500"
+        >
+          <span className="sr-only">Open main menu</span>
+          {isOpen ? (
+            <svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+            </svg>
+          ) : (
+            <svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
+            </svg>
+          )}
+        </button>
+      </section>
+      
+    </main>
+  );
 }
-
-
-
-// {isOpen && (
-//   <div className="sm:hidden">
-//     <div className="pt-2 pb-3 space-y-1">
-//       <Link href="/quem-somos" className="block px-3 py-2 text-base font-medium text-gray-900 hover:text-green-600">
-//         Quem somos
-//       </Link>
-//       <Link href="/solucoes" className="block px-3 py-2 text-base font-medium text-gray-900 hover:text-green-600">
-//         Soluções
-//       </Link>
-//       <Link href="/planos" className="block px-3 py-2 text-base font-medium text-gray-900 hover:text-green-600">
-//         Planos
-//       </Link>
-//       <Link href="/contato" className="block px-3 py-2 text-base font-medium text-gray-900 hover:text-green-600">
-//         Contato
-//       </Link>
-//       <Link href="/depoimentos" className="block px-3 py-2 text-base font-medium text-gray-900 hover:text-green-600">
-//         Depoimentos
-//       </Link>
-//       <Link href="/criar-conta" className="block px-3 py-2 text-base font-medium text-white bg-green-500 hover:bg-green-600">
-//         Criar conta
-//       </Link>
-//       <Link href="/acessar-conta" className="block px-3 py-2 text-base font-medium text-gray-900 hover:text-green-600">
-//         Acessar conta
-//       </Link>
-//     </div>
-//   </div>
-// )}
