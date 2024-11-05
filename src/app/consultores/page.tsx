4348dee49ea721062b29bd7a7fa75e8324294dbc
Pedro Bim <pedrodinizz2@outlook.com>
2024-11-05 11:40:23 -0300

    consultores page created

---
+++ src/app/consultores/page.tsx
@@ -0,0 +1,12 @@
+import FormSection from "@/components/FormSection";
+
+
+export default function Consultores(){
+  return (
+    <main className="flex flex-col">
+
+      <FormSection/>
+
+    </main>
+  )
+}
