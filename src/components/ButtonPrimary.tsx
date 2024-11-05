82da5aabb4672d262d8712110721981b2a342ed9
Pedro Bim <pedrodinizz2@outlook.com>
2024-11-05 11:42:11 -0300

    link function added

--- src/components/ButtonPrimary.tsx
+++ src/components/ButtonPrimary.tsx
@@ -1,27 +1,36 @@
 'use client'
 
 import { motion } from 'framer-motion';
+import Link from 'next/link'
+
+
 
 interface ButtonPrimaryProps {
   text: string;
-  onClick?: () => void;
+  href: string;
 }
 
-export default function ButtonPrimary({ text, onClick }: ButtonPrimaryProps) {
+export default function ButtonPrimary({ text, href }: ButtonPrimaryProps) {
+  const link = href ? href : "/";
+
   return (
+    <Link href={link}>
+
     <motion.button
-      transition={{ 
-        duration: 0.4, 
-        ease: "easeInOut", 
-        repeat: Infinity, 
-        repeatType: "loop",
-        repeatDelay: 1.4 
-      }}
-      animate={{ x: [0, 20, -20, 0] }} 
-      onClick={onClick}
-      className="px-4 py-2 text-white font-bold bg-[#01AB0D] rounded-3xl border-none hover:bg-green-600 transform hover:scale-110 transition-all duration-300 ease-in-out"
-    >
-      {text}
-    </motion.button>
+          transition={{ 
+            duration: 0.4, 
+            ease: "easeInOut", 
+            repeat: Infinity, 
+            repeatType: "loop",
+            repeatDelay: 1.4 
+          }}
+          animate={{ x: [0, 20, -20, 0] }} 
+          className="px-4 py-2 text-white font-bold bg-[#01AB0D] rounded-3xl border-none hover:bg-green-600 transform hover:scale-110 transition-all duration-300 ease-in-out"
+        >
+          {text}
+        </motion.button>
+    
+    </Link>
+   
   );
 }
