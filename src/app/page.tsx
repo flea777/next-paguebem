94fe4f41ce37837f6fa2b1fcedb71c5f7d51b1cc
Pedro Bim <pedrodinizz2@outlook.com>
2024-11-05 11:38:51 -0300

    formsection removed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
 import HeroSection from "@/components/HeroSection";
 import ProductsSection from "@/components/ProductsSection";
 import TestimonialSection from "@/components/TestimonialSection";
-import FormSection from "@/components/FormSection";
 import Carousel from "@/components/Carousel";
-
-
 
 export default function Home() {
   return (
@@ -14,7 +11,6 @@ export default function Home() {
       <Carousel />
       <ProductsSection/>
       <TestimonialSection/>
-      <FormSection />
     </main>
   );
 }
