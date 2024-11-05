a0b86ea39d1ee622640e22615a8df79777a803de
Pedro Bim <pedrodinizz2@outlook.com>
2024-11-05 11:41:06 -0300

    tag img changed for Image

--- src/components/PagueBemCard.tsx
+++ src/components/PagueBemCard.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Image from 'next/image';
 
 interface PagueBemCardProps {
   title: string;
@@ -11,7 +12,7 @@ const PagueBemCard: React.FC<PagueBemCardProps> = ({ title, description, imgUrl}
   return (
     <article className="flex">
       <div className="flex flex-col text-center justify-center items-center px-4 pt-5 pb-12 w-full rounded-3xl bg-rectangleGreen shadow-[4px_4px_4px_rgba(0,0,0,0.25)] overflow-hidden w-80 h-72">
-        <span><img src={imgUrl} alt={title}/></span>
+        <span><Image src={imgUrl} alt={title} width={80} height={80}/></span>
         <h2 className="font-bold font-spartan text-textGray mt-2 text-2xl tracking-tight leading-normal">{title}</h2>
         <p className="font-inter self-stretch text-textGray mt-7 text-xl tracking-tight leading-normal">{description}</p>
       </div>
