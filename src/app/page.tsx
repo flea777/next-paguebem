6b066d79f2a0f8f9d4f3d33c6bdd8e24a12b4a30
PriscilaMdeLima <Pml@cesar.school>
2024-11-13 14:07:24 -0300

    Criação do componente impacto pbb

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,13 @@ import TestimonialSection from "@/components/TestimonialSection";
 import Carousel from "@/components/Carousel";
 import FAQPage from "@/components/FAQPage";
 import Footer from "@/components/Footer";
+import ImpactosPBB from "../components/ImpactosPBB";
 
 export default function Home() {
   return (
     <main className="w-full h-screen mx-auto">
       <HeroSection />
+      <ImpactosPBB />
       <div />
       <Carousel />
       <ProductsSection />
