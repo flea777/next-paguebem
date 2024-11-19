f39a5018eeb980f7dfb0d7e79d886595a9e22cd0
Viniciusgithu <viniciuslimavirtuel@email.com>
2024-11-19 11:13:55 -0300

    add responsividade heroSection, buttonPrimary and buttonSecondary

--- src/components/ButtonPrimary.tsx
+++ src/components/ButtonPrimary.tsx
@@ -8,10 +8,12 @@ import Link from 'next/link'
 interface ButtonPrimaryProps {
   text: string;
   href: string;
+  className?: string;
 }
 
-export default function ButtonPrimary({ text, href }: ButtonPrimaryProps) {
+export default function ButtonPrimary({ text, href, className }: ButtonPrimaryProps) {
   const link = href ? href : "/";
+  const buttonClass = `px-4 py-2 text-white font-bold bg-[#01AB0D] rounded-3xl border-none hover:bg-green-600 transform hover:scale-110 transition-all duration-300 ease-in-out ${className}`;
 
   return (
     <Link href={link}>
@@ -25,7 +27,7 @@ export default function ButtonPrimary({ text, href }: ButtonPrimaryProps) {
             repeatDelay: 1.4 
           }}
           animate={{ x: [0, 20, -20, 0] }} 
-          className="px-4 py-2 text-white font-bold bg-[#01AB0D] rounded-3xl border-none hover:bg-green-600 transform hover:scale-110 transition-all duration-300 ease-in-out"
+          className={buttonClass}
         >
           {text}
         </motion.button>
