93de5b86b9336e21bd1bb250ebf8545202ada150
Isaac-Daniel-A-D <166683272+Isaac-Daniel-A-D@users.noreply.github.com>
2024-11-22 14:54:50 -0300

    Update PagueBemCard.tsx

--- src/components/PagueBemCard.tsx
+++ src/components/PagueBemCard.tsx
@@ -11,10 +11,10 @@ const PagueBemCard: React.FC<PagueBemCardProps> = ({ title, description, imgUrl}
 
   return (
     <article className="flex">
-      <div className="flex flex-col text-center justify-center items-center px-4 pt-5 pb-12 w-full rounded-3xl bg-rectangleGreen shadow-[4px_4px_4px_rgba(0,0,0,0.25)] overflow-hidden w-80 h-72">
+      <div className="flex flex-col text-center justify-center items-center px-4 pt-5 pb-12 w-full rounded-3xl bg-rectangleGreen shadow-[4px_4px_4px_rgba(0,0,0,0.25)] overflow-hidden">
         <span><Image src={imgUrl} alt={title} width={80} height={80}/></span>
-        <h2 className="font-bold font-spartan text-textGray mt-2 text-2xl tracking-tight leading-normal">{title}</h2>
-        <p className="font-inter self-stretch text-textGray mt-7 text-xl tracking-tight leading-normal">{description}</p>
+        <h2 className="font-bold font-spartan text-textGray mt-2 text-2xl tracking-tight leading-normal overflow-hidden">{title}</h2>
+        <p className="font-inter self-stretch text-textGray mt-7 text-xl tracking-tight leading-normal overflow-hidden">{description}</p>
       </div>
     </article>
   );
